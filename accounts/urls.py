0bb7136737fa453bffb18be51ac83d2284db0159
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-08-12 23:54:49 +0530

    student model created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentProfileView
 
 router = DefaultRouter()
-router.register(r'student', StudentProfileView, basename='student')
 
 urlpatterns = [
 ] + router.urls
--- students/models.py
+++ students/models.py
@@ -1,25 +1,47 @@
 import uuid
 from django.db import models
 
+SCHOOLING_MEDIUM = (
+    ('GUJ', 'Gujarati'),
+    ('ENG', 'English'),
+    ('HN', 'Hindi'),
+)
+
 # Create your models here.
-class StudentProfileModel(models.Model):
+class StudentPersonalDetailModel(models.Model):
     id = models.UUIDField(primary_key= True, default= uuid.uuid4, auto_created= True, editable= False)
     firstName = models.CharField(max_length= 50)
     lastName = models.CharField(max_length= 50)
     mobile = models.CharField(max_length= 50)
     whatsappMobile = models.CharField(max_length= 50)
     alternateMobile = models.CharField(max_length= 50)
+    email = models.EmailField(max_length=100, unique=True)
     address = models.TextField()
     city = models.CharField(max_length= 50)
+    isVerified = models.BooleanField(default=False)
+    isBlocked = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updatedAt = models.DateTimeField(auto_now_add=False, auto_now=True)
     
-class StudentEducationModel(models.Model):
-    SCHOOLING_MEDIUM = (
-        ('GUJ', 'Gujarati'),
-        ('ENG', 'English'),
-        ('HN', 'Hindi'),
-    )
-    
+class StudentSchoolDetailModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, auto_created=True, editable=False)
-    studentId = models.ForeignKey(StudentProfileModel, on_delete=models.CASCADE)
-    
+    studentId = models.OneToOneField(StudentPersonalDetailModel, on_delete=models.CASCADE)
     schoolingMedium = models.CharField(max_length= 50, choices= SCHOOLING_MEDIUM)
+    twelvePercent = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    diplomaPercent = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    tenPercent = models.DecimalField(max_digits=4, decimal_places=2)
+    createdAt = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updatedAt = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+class StudentCollegeDetailModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, auto_created=True, editable=False)
+    studentId = models.OneToOneField(StudentPersonalDetailModel, on_delete=models.CASCADE)
+    enrollmentNumber = models.CharField(max_length=15, unique=True)
+    college = models.CharField(max_length=15)
+    branch = models.CharField(max_length=15)
+    passingYear = models.IntegerField()
+    currentCGPA = models.DecimalField(max_digits=3, decimal_places=2)
+    currentBacklog = models.IntegerField()
+    totalBacklog = models.IntegerField()
+    createdAt = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updatedAt = models.DateTimeField(auto_now_add=False, auto_now=True)
