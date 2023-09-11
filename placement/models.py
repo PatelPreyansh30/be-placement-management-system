1b582cb1c9b3c714632ee411ebb6a22f13ea1c41
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-09-11 23:17:32 +0530

    apply api change

--- placement/models.py
+++ placement/models.py
@@ -1,9 +1,8 @@
 import uuid
 from django.db import models
 from companies.models import CompanyModel
-from django.contrib.auth import get_user_model
+from students.models import StudentModel
 
-UserModel = get_user_model()
 
 class PlacementApplicationModel(models.Model):
 
@@ -13,11 +12,14 @@ class PlacementApplicationModel(models.Model):
         ('placed', 'Placed'),
     ]
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,auto_created=True,editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          auto_created=True, editable=False)
     studentId = models.OneToOneField(
-        UserModel, on_delete=models.CASCADE, related_name="studentApplicationDetail")
-    companyId = models.ForeignKey(CompanyModel, on_delete=models.CASCADE)
-    status = models.CharField(choices=STATUS_CHOICES, max_length=20, default='applied')
+        StudentModel, on_delete=models.CASCADE, related_name="appliedStudent")
+    companyId = models.ForeignKey(
+        CompanyModel, on_delete=models.CASCADE, related_name="appliedCompany")
+    status = models.CharField(choices=STATUS_CHOICES,
+                              max_length=20, default='applied')
     appliedAt = models.DateTimeField(auto_now_add=True, auto_now=False)
     createdAt = models.DateTimeField(auto_now_add=True, auto_now=False)
     updatedAt = models.DateTimeField(auto_now_add=False, auto_now=True)
--- placement/views.py
+++ placement/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework import mixins, viewsets, permissions
-from . import models,serializer
+from . import models, serializer
 
 # Create your views here.
-class PlacementApplicationView(viewsets.ModelViewSet):
+
+
+class PlacementApplicationView(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = models.PlacementApplicationModel.objects.all()
     serializer_class = serializer.PlacementApplicationSerializer
-    
     # permission_classes = [permissions.IsAuthenticated]
