41e13ca0748a00e830a495eb96f47cafd41ef914
Rushikeshf01 <falakrushikesh83@gmail.com>
2023-09-14 23:59:18 +0530

    applicatin list api for studentand company created

--- placement/urls.py
+++ placement/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import PlacementApplicationView
+from .views import PlacementApplicationView,ApplicationTrackingView
 
 router = DefaultRouter()
-router.register('apply', PlacementApplicationView)
+router.register('apply', PlacementApplicationView, basename='placement-apply')
+router.register('application', ApplicationTrackingView,
+                basename='placement-application')
 
 urlpatterns = [
     
--- placement/views.py
+++ placement/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render
 from rest_framework import mixins, viewsets, permissions
 from . import models, serializer
 
-# Create your views here.
-
-
 class PlacementApplicationView(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = models.PlacementApplicationModel.objects.all()
     serializer_class = serializer.PlacementApplicationSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+class ApplicationTrackingView(mixins.ListModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    serializer_class = serializer.PlacementApplicationSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        student_id = self.request.query_params.get('studentApplication')
+        comp_id = self.request.query_params.get('companyApplication')
+
+        if student_id:
+            return models.PlacementApplicationModel.objects.select_related('companyId').filter(studentId=student_id).all()
+        if comp_id:
+            return models.PlacementApplicationModel.objects.filter(companyId=comp_id).all()
+        
+        return models.PlacementApplicationModel.objects.all()
